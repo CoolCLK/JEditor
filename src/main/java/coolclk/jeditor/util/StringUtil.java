4ac9a714352f7863084a15489e68e99ef8875383
CoolCLK <mr.chenlk123@qq.com>
2024-02-15 19:31:36 +0800

    Fix replace util buggy

--- src/main/java/coolclk/jeditor/util/StringUtil.java
+++ src/main/java/coolclk/jeditor/util/StringUtil.java
@@ -11,7 +11,7 @@ public class StringUtil {
      * @author CoolCLK
      */
     public static String replace(Object target, Object... replacements) {
-        return replace(target, 1, replacements);
+        return replace0(target, 1, replacements);
     }
 
     /**
@@ -22,7 +22,7 @@ public class StringUtil {
      * @author CoolCLK
      */
     public static String replaceLast(Object target, Object... replacements) {
-        return replace(target, -1, replacements);
+        return replace0(target, -1, replacements);
     }
 
     /**
@@ -33,32 +33,31 @@ public class StringUtil {
      * @author CoolCLK
      */
     public static String replaceAll(Object target, Object... replacements) {
-        return replace(target, 0, replacements);
+        return replace0(target, 0, replacements);
     }
 
-    private static String replace(Object target, int limit, Object... replacements) {
+    private static String replace0(Object target, int limit, Object... replacements) {
         if (Objects.isNull(target) || target.toString().isEmpty()) {
             return "";
         }
         if (replacements.length % 2 == 0) {
             String result = target.toString();
             for (int i = 0; i < replacements.length; i += 2) {
-                result = replace(result, replacements[i].toString(), replacements[i + 1].toString(), limit);
+                result = replace1(result, replacements[i].toString(), replacements[i + 1].toString(), limit);
             }
             return result;
         }
         return null;
     }
 
-    private static String replace(String target, String regex, String replacement, int limit) {
+    private static String replace1(String target, String regex, String replacement, int limit) {
         if (!target.isEmpty() && !regex.isEmpty()) {
             StringBuilder result = new StringBuilder();
-            String[] targetCharacters = target.split("");
-            String[] regexCharacters = regex.split("");
+            char[] targetCharacters = target.toCharArray();
+            char[] regexCharacters = regex.toCharArray();
             int replaceCounter = 0;
-            for (int i = (limit > 0 ? 0 : targetCharacters.length - 1); (limit > 0 ? i < targetCharacters.length : i >= 0); i += (limit > 0 ? 1 : -1)) {
-                result.append(targetCharacters[i]);
-                if (Objects.equals(targetCharacters[0], regexCharacters[0])) {
+            for (int i = (limit >= 0 ? 0 : targetCharacters.length - 1); (limit >= 0 ? i < targetCharacters.length : i >= 0); i += (limit >= 0 ? 1 : -1)) {
+                if (Objects.equals(targetCharacters[i], regexCharacters[0])) {
                     boolean match = true;
                     for (int c = 0; i + c < targetCharacters.length && c < regexCharacters.length; c++) {
                         if (!Objects.equals(targetCharacters[i + c], regexCharacters[c])) {
@@ -68,12 +67,14 @@ public class StringUtil {
                     }
                     if (match) {
                         result.append(replacement);
-                        i += (limit > 0 ? 1 : -1) * regexCharacters.length;
+                        i += (limit >= 0 ? 1 : -1) * regexCharacters.length;
                         replaceCounter++;
                         if (limit != 0 && replaceCounter >= Math.abs(limit)) {
                             break;
                         }
                     }
+                } else {
+                    result.append(targetCharacters[i]);
                 }
             }
             return result.toString();
